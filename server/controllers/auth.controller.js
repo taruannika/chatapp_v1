eb56e3bf3b95b98d7637c0d109016f10dcefec1a
HamalainenTaru <thamalainen1990@gmail.com>
2025-05-22 20:57:14 +0300

    backend: made authroutes, authcontroller and usermodel templates

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,3 @@
+export const signupUser = async (request, response, next) => {};
+export const loginUser = async (request, response, next) => {};
+export const logoutUser = (request, response, next) => {};
--- server/index.js
+++ server/index.js
@@ -6,11 +6,15 @@ import { NODE_ENV, PORT } from "./utils/config.js";
 import { connectToMongoDB } from "./db/connectToMongoDB.js";
 import { successLog } from "./utils/logger.js";
 
+import authRoutes from "./routes/auth.route.js";
+
 const app = express();
 
 if (NODE_ENV === "development") app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 connectToMongoDB().then(() => {
   app.listen(PORT, () => {
