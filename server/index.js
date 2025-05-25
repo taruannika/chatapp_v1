01ab17465e3516f412ed116a9ab88351581132b4
HamalainenTaru <thamalainen1990@gmail.com>
2025-05-25 21:08:53 +0300

    backend: usermodel and errorhandler middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
-
 import { NODE_ENV, PORT } from "./utils/config.js";
 import { connectToMongoDB } from "./db/connectToMongoDB.js";
 import { successLog } from "./utils/logger.js";
-
+import { errorhandler } from "./middlewares/errorhandler.js";
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
@@ -15,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use(errorhandler);
 
 connectToMongoDB().then(() => {
   app.listen(PORT, () => {
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,31 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema(
+  {
+    email: String,
+    username: String,
+    password: String,
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified(this.password)) return next();
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
+
+userSchema.set("toJSON", {
+  transform: (doc, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+    delete returnedObj.password;
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 
